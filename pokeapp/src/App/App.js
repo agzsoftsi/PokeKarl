d18e42a10ab2e20336f40f946108f6cb41bb00bf
Carlos Garcia <agzsoftsi@gmail.com>
2021-04-20 21:39:40 -0500

    Search bar, search button and speech button implemented only UI

--- pokeapp/src/App/App.js
+++ pokeapp/src/App/App.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
+  const urlAPI = 'https://pokeapi.co/api/v2/pokemon/';
   const [skin, setSkin] = useState(0);
   const changeSkin = () => {
       setSkin(skin<1?1:0);  
-      }
+    }
 
   return (
     <div className='App'>
@@ -18,6 +18,25 @@ function App() {
           <span id='icon-b1' className={'icon-button-'+skin} title={'Change to '+ (skin===0?'Dark':'Red') +' skin'}> </span>
         </button>
       </div>
+
+      <div className='Search-bar'>
+        <input type='text' id='searchPokemon' className='input-search'/>
+      </div>
+
+      <div>
+        <button className='ButtonSpeech' onClick = ''>
+          <span className='icon-speech' title='Speech PokeInfo'> </span>
+        </button>
+      </div>
+
+      <div>
+        <button className='ButtonSearch' onClick = ''>
+          <span className='icon-search' title='Search Pokemon'> </span>
+        </button>
+      </div>
+      
+
+      
       
      
     </div>
